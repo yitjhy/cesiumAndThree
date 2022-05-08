150fcfcc92f76294d3d682089a44fbfe02d8515c
yitjhy <yitjhy@users.noreply.github.com>
2022-05-08 16:12:59 +0000

    deploy: 05c8accdb33898b6c92d2db8a67f11584ff442e4

---
+++ precache-manifest.2df4f6512acacb878edfc6e602a985b9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063692dc155ed79f70d1e0de5f2aaed6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76a3cf1e40e3b1a41440",
+    "url": "./static/css/2.6750e1b9.chunk.css"
+  },
+  {
+    "revision": "ccab708ea59bf57709f0",
+    "url": "./static/css/main.7fd75db2.chunk.css"
+  },
+  {
+    "revision": "76a3cf1e40e3b1a41440",
+    "url": "./static/js/2.9932b157.chunk.js"
+  },
+  {
+    "revision": "ccab708ea59bf57709f0",
+    "url": "./static/js/main.c4941b1c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5f4ff75c5abb15cf11ee7a4140747468",
+    "url": "./static/media/Earth.5f4ff75c.png"
+  },
+  {
+    "revision": "04301e1c505f905390fb68d2478d1b49",
+    "url": "./static/media/background_black.04301e1c.png"
+  },
+  {
+    "revision": "12a325e41a11bec72c8adaef67ed16ee",
+    "url": "./static/media/negx.12a325e4.jpg"
+  },
+  {
+    "revision": "51d75180e1fd0446f9f3186c36688715",
+    "url": "./static/media/negy.51d75180.jpg"
+  },
+  {
+    "revision": "3b16ecc0c1b3f6d2eef3aa211066c46e",
+    "url": "./static/media/negz.3b16ecc0.jpg"
+  },
+  {
+    "revision": "32dd322262b2cd7ace66a7c9dbb51a60",
+    "url": "./static/media/posx.32dd3222.jpg"
+  },
+  {
+    "revision": "f14a66f798a100faacba54df77b5c9c3",
+    "url": "./static/media/posy.f14a66f7.jpg"
+  },
+  {
+    "revision": "ecd13cfaf79cecc2579381048b2fd531",
+    "url": "./static/media/posz.ecd13cfa.jpg"
+  },
+  {
+    "revision": "cfe2698c6be15936919ee31f53373c25",
+    "url": "./static/media/rack.cfe2698c.jpg"
+  },
+  {
+    "revision": "15be1a958b42c45d91b3a6cce5816f44",
+    "url": "./static/media/rack_door_back.15be1a95.jpg"
+  },
+  {
+    "revision": "109bd5a5899e07a85cb36de89b006300",
+    "url": "./static/media/rack_front_door.109bd5a5.jpg"
+  },
+  {
+    "revision": "197c2190760fb50922871e10c834ef3e",
+    "url": "./static/media/rack_inside.197c2190.jpg"
+  }
+]);
