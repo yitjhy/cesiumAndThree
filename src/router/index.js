6712aa43fde944ed3925050f10821ceaca177381
yitjhy@126.com <yitjhy@126.com>
2022-05-08 12:18:17 +0800

    重新配置gitpage地图地址

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+import {Route, HashRouter as Router, Switch} from 'react-router-dom';
 import Canvas from './../pages/canvas/canvas';
 import App from './../App';
 
